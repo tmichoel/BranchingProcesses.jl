5d10068d9f557a12cd431b44810eead43599ae9e
tmichoel <11647967+tmichoel@users.noreply.github.com>
2025-04-19 11:39:01 +0530

    add deps

--- src/BranchingProcesses.jl
+++ src/BranchingProcesses.jl
@@ -1,5 +1,17 @@
 module BranchingProcesses
 
-# Write your package code here.
+# dependencies
+using Distributions
+using AbstractTrees
+using Accessors
+using DifferentialEquations
+using JumpProcesses
+using Catalyst
+
+# include files
+include("types.jl")
+include("initializers.jl")
+include("solvers.jl")
+include("plot_recipes.jl")
 
 end
