65d4d83fc0b90f715f707cd9d94fcdb7b4dae4e2
tmichoel <11647967+tmichoel@users.noreply.github.com>
2025-05-24 13:14:00 +0530

    add plot recipes for branching process solutions

---
+++ src/plot_recipes.jl
@@ -0,0 +1,25 @@
+@recipe function f(tree::BranchingProcessSolution)
+    # set a default value for some attributes
+    xlabel --> "t"
+    ylabel --> "u"
+    legend --> false
+    # collect the nodes of the tree in pre-order
+    nodes = collect(PreOrderDFS(tree))
+    # create a series for each node
+    for node in nodes
+        @series begin
+            node.sol.t, node.sol.u
+        end 
+    end
+end
+
+@recipe function f(tree::SimTree)
+    # collect the nodes of the tree in pre-order
+    nodes = collect(PreOrderDFS(tree))
+    # create a series for each node
+    for node in nodes
+        @series begin
+            node.t, node.u
+        end 
+    end
+end
