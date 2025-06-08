daef28c2e7771c7b8d810b799432b7de9725c9b0
tmichoel <11647967+tmichoel@users.noreply.github.com>
2025-06-08 10:52:49 +0200

    force path series type

--- src/plot_recipes.jl
+++ src/plot_recipes.jl
@@ -5,10 +5,25 @@
     legend --> false
     # collect the nodes of the tree in pre-order
     nodes = collect(PreOrderDFS(tree))
-    # create a series for each node
+    # create a path series with trajectories for each node
     for node in nodes
         @series begin
+            seriestype := :path
             node.sol.t, node.sol.u
         end 
     end
+    # optionally add markers at the branch points
+    # if add_branchpoints
+    #     @series begin
+    #         seriestype := :scatter
+    #         markercolor := :red
+    #         markerstrokecolor := :black
+    #         markersize := 7
+    #         delete!(plotattributes, :add_marker)
+    #         # collect the branch points
+    #         branch_points_t = [node.sol.t[end] for node in nodes]
+    #         branch_points_u = [node.sol.u[end] for node in nodes]
+    #         branch_points_t, branch_points_u
+    #     end
+    # end
 end
