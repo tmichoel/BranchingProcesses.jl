aee1f81829ebc9ef5e3e7e3738951ebb5746ddc9
tmichoel <11647967+tmichoel@users.noreply.github.com>
2025-06-13 14:10:36 +0200

    fix xlims

--- src/plot_recipes.jl
+++ src/plot_recipes.jl
@@ -5,12 +5,16 @@
     legend --> false
     # collect the nodes of the tree in pre-order
     nodes = collect(PreOrderDFS(tree))
+    # find the total timespan of the solution
+    tmin = tree.sol.t[1]
+    tmax = maximum([node.sol.t[end] for node in nodes])
     # create a path series with trajectories for each node
     for node in nodes
         @series begin
             seriestype := :path
             linewidth --> 2
-            node.sol.t, node.sol.u
+            #node.sol.t, node.sol.u
+            node.sol
         end 
     end
     # optionally add markers at the branch points
@@ -20,11 +24,13 @@
             markercolor --> :black
             markerstrokecolor --> :black
             markersize --> 5
-            #delete!(plotattributes, :add_branchpoints)
             # collect the branch points
             branch_points_t = [node.sol.t[1] for node in nodes]
-            branch_points_u = [node.sol.u[1] for node in nodes]
+            branch_points_u = [node.sol[1,:][1] for node in nodes]
             branch_points_t, branch_points_u
         end
     end
+    # set the axis limits|
+    xlims --> [tmin, tmax]
+    widen --> true
 end
