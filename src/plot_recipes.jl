02a7d1851660967146758ad85e08f42734612217
tmichoel <11647967+tmichoel@users.noreply.github.com>
2025-07-04 21:20:53 +0200

    support multiple variables in the plot recipes for BranchingProcessSolution objects.

--- src/plot_recipes.jl
+++ src/plot_recipes.jl
@@ -1,4 +1,4 @@
-@recipe function f(tree::T; add_branchpoints=false) where T <: BranchingProcessSolution
+@recipe function f(tree::T; add_branchpoints=false, idxs=[1]) where T <: BranchingProcessSolution
     # set a default value for some attributes
     xlabel --> "t"
     ylabel --> "u"
@@ -8,26 +8,29 @@
     # find the total timespan of the solution
     tmin = tree.sol.t[1]
     tmax = maximum([node.sol.t[end] for node in nodes])
-    # create a path series with trajectories for each node
+    # create a path series with trajectories for each node for all variables in idxs
     for node in nodes
         @series begin
             seriestype := :path
             linewidth --> 2
-            #node.sol.t, node.sol.u
+            idxs --> idxs
             node.sol
         end 
     end
-    # optionally add markers at the branch points
+   
+    # optionally add markers at the branch points for all variables in idxs
     if add_branchpoints
-        @series begin
-            seriestype := :scatter
-            markercolor --> :black
-            markerstrokecolor --> :black
-            markersize --> 5
-            # collect the branch points
-            branch_points_t = [node.sol.t[1] for node in nodes]
-            branch_points_u = [node.sol[1,:][1] for node in nodes]
-            branch_points_t, branch_points_u
+        for k in idxs
+            @series begin
+                seriestype := :scatter
+                markercolor --> :black
+                markerstrokecolor --> :black
+                markersize --> 5
+                # collect the branch points
+                branch_points_t = [node.sol.t[1] for node in nodes]
+                branch_points_u = [node.sol[k,:][1] for node in nodes]
+                branch_points_t, branch_points_u
+            end
         end
     end
     # set the axis limits|
