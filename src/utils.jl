6ea0435121704e563ddac9c823ab71ae85174bda
Tom Michoel <11647967+tmichoel@users.noreply.github.com>
2025-10-14 14:51:26 +0200

    add tree reduction (not implemented yet)

--- src/utils.jl
+++ src/utils.jl
@@ -13,15 +13,19 @@ end
 """
     reduce_tree(tree::BranchingProcessSolution)
 
-Reduce [`BranchingProcessSolution`](@ref) `tree` to an ordinary time series by combining the values of all particles alive at 
-"""
-# function reduce_tree(tree::BranchingProcessSolution,tspan,dt)
-#     t = tspan[1]:dt:tspan[2]
-#     u = zeros(size(t))
-#     for node in PostOrderDFS(tree)
+Reduce [`BranchingProcessSolution`](@ref) `tree` to an ordinary time series by combining the values of all particles alive at each time point. The timespan of the resulting time series is from the initial time of the root to the latest final time of any of the leaves of the input `tree`; the time points are spaced by `dt` (default: `0.01`). The function `reduction` (default: `sum`) is used to combine the values of all particles alive at each time point.
 
-#     end
-# end
+!!! warning
+    The `reduction` function must be computable in an iterative manner, that is, it must be possible to compute the value of `reduction(a, b)` given only the values of `a` and `b`, without needing to know the values of all previous arguments. Examples of such functions are `sum`, `prod`, `maximum`, and `minimum`. Functions that are not computable in an iterative manner include, for example, `mean` and `median`. 
+"""
+function reduce_tree(tree::BranchingProcessSolution; reduction=sum, dt=0.01)
+    tspan = get_timespan(tree)
+    trange = tspan[1]:dt:tspan[2]
+    u = zeros(length(trange),length(tree.sol.u[1]))
+    for node in PostOrderDFS(tree)
+
+    end
+end
 
 
 """
