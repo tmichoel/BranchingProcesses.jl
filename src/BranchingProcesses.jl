f95c9d5249b16045f68c828c65c2cc6b7f05a5b1
tmichoel <11647967+tmichoel@users.noreply.github.com>
2025-10-20 20:23:46 +0200

    fixing issues with ReducedTreeSolution

--- src/BranchingProcesses.jl
+++ src/BranchingProcesses.jl
@@ -13,16 +13,23 @@ module BranchingProcesses
 
 # dependencies
 using AbstractTrees
+using DataInterpolationsND
 using Distributions
 using DocStringExtensions
 using RecipesBase
 using SciMLBase
-using DataInterpolations
+
 
 # include files
 include("types.jl")
 include("solvers.jl")
 include("plot_recipes.jl")
 include("utils.jl")
+
+# exported names
+export ConstantRateBranchingProblem, BranchingProcessSolution #, ReducedBranchingProcessSolution
+export solve
+export tip_values, get_timespan #, reduce_tree
+
 
 end
